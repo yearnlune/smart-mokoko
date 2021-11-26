03f6904b39b1d41e1b226fe08652c06427224737
yearnlune <kdhpopoya@gmail.com>
2021-11-26 17:51:32 +0900

    feat: Added `MarketCategoryTypes`

---
+++ src/main/kotlin/lab/yearnlune/model/type/MarketCategoryTypes.kt
@@ -0,0 +1,19 @@
+package lab.yearnlune.model.type
+
+enum class MarketCategoryTypes(val value: Int) {
+
+    ALL(1),
+    EQUIPMENT_CHESTS(2),
+    AVATAR(3),
+    SEAL_BOOKS(4),
+    ENHANCEMENT_MATERIALS(5),
+    COMBAT_GOODS(6),
+    FOOD(7),
+    LIFE(8),
+    ADVENTURE_BOOK(9),
+    VOYAGE(10),
+    PETS(11),
+    MOUNTS(12),
+    MISC(13),
+    JEWELS_CHESTS(14)
+}
